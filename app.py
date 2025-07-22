a1d9ac5e535f5b3110d77a0aaafdba5f5ecff88c
Jaya Sakthi.G <sakthij697@gmai.com>
2025-07-22 19:34:27 +0530

    learn

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 def home():
     return """
     <center><h1> Welcome to My Flask App!</h1><br><br>
-    <p><h2>Flask is a micro web framework for Python, used for building web applications. It is classified as a "microframework" because it provides a lightweight core with essential functionalities for web development, such as URL routing and template rendering, but it does not include built-in features like database abstraction layers, form validation, or full-fledged authentication systems. Instead, Flask emphasizes flexibility and allows developers to choose and integrate specific libraries and extensions for these functionalities as needed.</h2></p></center>
+    <p><h2>Flask is a micro web framework for Python, used for building web applications. It is classified as a "microframework" because it provides a lightweight core with essential functionalities for web development, such as URL routing and template rendering, but it does not include built-in features like database abstraction layers, form validation, or full-fledged authentication systems. Instead, Flask emphasizes flexibility and allows developers to choose and integrate specific libraries and extensions for these functionalities as needed.<br><br>Thank you</h2></p></center>
     
     """
 
